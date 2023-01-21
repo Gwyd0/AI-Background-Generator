657fdc2d06bb192eac21fa9b52b25dfeeee766bf
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2023-01-21 14:37:48 +0000

    crops images.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def settings():
         return api, directory
 
 
-def genImage(imageDirectory, prompt):  # gens an image using openai dalle and then downloads it.
+def genImage(imageDirectory, prompt, size):  # gens an image using openai dalle and then downloads it.
     response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
     image_url = response['data'][0]['url']
 
@@ -31,18 +31,21 @@ def genImage(imageDirectory, prompt):  # gens an image using openai dalle and th
     urllib.request.urlretrieve(image_url, imageDirectory)
     print("> Generated image.")
     image = Image.open(imageDirectory)
-    cropped_img = image.crop((0, 0, 576, 1024))
-    cropped_img.show()
-    print("> Shown Image")
+    if size == "p":
+        cropped_img = image.crop((0, 0, 576, 1024))
+        cropped_img.show()
+    else:
+        image.show()
+        print("> Shown Image")
 
 
 apikey, directory = settings()
 
 openai.api_key = apikey
 
-print("---- AI Desktop Wallpaper Generator ---- \nBy Gwyd\nCurrently this can generate (C)itys, (N)ature and cursed ("
-      "A)nime backgrounds.")
-option = input("Please input an option\n> ").lower()
+option = input(
+    "---- AI Desktop Wallpaper Generator ---- \nBy Gwyd\nCurrently this can generate (C)itys, (N)ature and cursed ("
+    "A)nime or (CU)stom.\nPlease input an option\n> ").lower()
 
 if option == "c":
     finalPrompt = "City digital art realistic photo desktop wallpaper detailed portrait"
@@ -50,9 +53,17 @@ elif option == "n":
     finalPrompt = "Nature forest  desktop wallpaper detailed portrait"
 elif option == "a":
     finalPrompt = "anime digital art photo desktop wallpaper detailed portrait"
+elif option == "cu":
+    finalPrompt = input("Input your custom dall-e prompt\n> ")
 else:
     print("> That is not an option.")
     exit()
 
-print("> Please wait . . .")
-genImage(directory, finalPrompt)
+option1 = input("Sizes (l)andscape, (p)ortrait or (N)one - this will keep the original size.\nPlease input an "
+                "option\n> ").lower()
+if not option1 == "l" and not option1 == "p" and not option1 == "n":
+    print("> That is not an option.")
+    exit()
+else:
+    print("> Please wait . . .")
+    genImage(directory, finalPrompt, option1)
